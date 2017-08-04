7fcf82559a216d2bcd923851a36066937e13b972
김만중 <mjk@gimmanjung-ui-MacBook-Pro.local>
2017-08-04 17:40:41 +0900

    [호근]HeartActivity,SettingActivity 추가

--- app/src/main/java/com/hodo/jjamtalk/MyProfile.java
+++ app/src/main/java/com/hodo/jjamtalk/MyProfile.java
@@ -1,17 +1,42 @@
 package com.hodo.jjamtalk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by mjk on 2017. 8. 4..
  */
 
 public class MyProfile extends AppCompatActivity {
+
+    ImageButton btn_Setting;
+    Button btn_heart;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile_from_main);
+
+        btn_Setting = (ImageButton)findViewById(R.id.btn_setting);
+        btn_Setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SettingActivity.class));
+
+            }
+        });
+        btn_heart = (Button)findViewById(R.id.btn_heart);
+        btn_heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),HeartActivity.class));
+            }
+        });
+
     }
 }
