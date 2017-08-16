5c3978f9603dc99b4d2cfd9c92d3a7328c0e9442
김만중 <mjk@gimmanjung-ui-MacBook-Pro.local>
2017-08-16 18:51:51 +0900

    [호근]타이틀바 변경

--- app/src/main/java/com/hodo/jjamtalk/BoardItemActivity.java
+++ app/src/main/java/com/hodo/jjamtalk/BoardItemActivity.java
@@ -1,6 +1,5 @@
 package com.hodo.jjamtalk;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +7,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +29,8 @@ public class BoardItemActivity extends AppCompatActivity{
     ImageButton ib_vote_like,ib_warn;
     EditText et_reply;
     LinearLayout imageViewLayout;
+    Toolbar toolbar;
+
 
 
 
@@ -35,7 +39,21 @@ public class BoardItemActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_item);
 
-        final Activity mActivity = this;
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //toolbar.setTitle("허허참");
+        //toolbar.setTitleTextColor(Color.parseColor("#ff0099cc"));
+
+        //setSupportActionBar(toolbar);
+
+
+
+
+
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.board_icon);
+        //setSupportActionBar(toolbar);
+
+
         btn_send = (Button) findViewById(R.id.btn_send);
 
         ib_vote_like = (ImageButton) findViewById(R.id.ib_vote_like);
@@ -45,30 +63,7 @@ public class BoardItemActivity extends AppCompatActivity{
                 Toast.makeText(getApplicationContext(),"좋아요를 눌렀습니다",Toast.LENGTH_SHORT).show();
             }
         });
-        ib_warn = (ImageButton) findViewById(R.id.ib_warn);
-        ib_warn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                AlertDialog.Builder builder= new AlertDialog.Builder(mActivity);
-                builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                }).
-                setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                }).setMessage("이 사람을 신고하시겠습니까?")
-                        .setTitle("신고하기");
-                AlertDialog dialog = builder.create();
-                dialog.show();
-
-            }
-        });
         et_reply = (EditText)findViewById(R.id.et_reply);
 
         recyclerView_board_reply = (RecyclerView)findViewById(R.id.recyclerview_board_reply);
@@ -83,6 +78,37 @@ public class BoardItemActivity extends AppCompatActivity{
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.board_item,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btn_report:
+                AlertDialog.Builder builder= new AlertDialog.Builder(this);
+                builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).
+                        setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }).setMessage("이 사람을 신고하시겠습니까?")
+                        .setTitle("신고하기");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+
+        }
+        return true;
     }
 
 }
