61b749bc85835d8b4dfe850972a8f2c52d7f6138
MJK-PC\mjk <hngpic@gmail.com>
2017-09-22 10:48:52 +0900

    [호근]보석선물 작업중

--- app/src/main/java/com/hodo/jjamtalk/MyJewelAdapter.java
+++ app/src/main/java/com/hodo/jjamtalk/MyJewelAdapter.java
@@ -5,6 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hodo.jjamtalk.ViewHolder.MyJewelViewHolder;
 
 /**
  * Created by mjk on 2017-09-21.
@@ -12,6 +16,7 @@ import android.widget.BaseAdapter;
 
 class MyJewelAdapter extends BaseAdapter {
     Context mContext;
+
 
     public MyJewelAdapter(Context context) {
         super();
@@ -35,7 +40,13 @@ class MyJewelAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+
         View v = LayoutInflater.from(mContext).inflate(R.layout.item_dialog_my_jewel_list,viewGroup,false);
+        ImageView iv =v.findViewById(R.id.iv_jewel);
+        TextView tv = v.findViewById(R.id.tv_count);
+        iv.setImageResource(R.drawable.gold);
+        tv.setText("x3");
+
         return v;
     }
 }
--- app/src/main/java/com/hodo/jjamtalk/UserPageActivity.java
+++ app/src/main/java/com/hodo/jjamtalk/UserPageActivity.java
@@ -5,24 +5,27 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayout;
+
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,7 +43,9 @@ import com.hodo.jjamtalk.Data.UserData;
 import com.hodo.jjamtalk.Firebase.FirebaseData;
 import com.hodo.jjamtalk.Util.NotiFunc;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Created by mjk on 2017. 8. 5..
@@ -197,8 +202,18 @@ public class UserPageActivity extends AppCompatActivity {
                         break;
                     case R.id.UserPage_btnGiftJewel:
                         View v = inflater.inflate(R.layout.dialog_give_jewel,null);
-                        GridView gridView = v.findViewById(R.id.gv_myjewel);
-                        gridView.setAdapter(myjewelAdapter);
+                        Spinner sp_jewel = v.findViewById(R.id.sp_jewel);
+                        /*int [] intArray  =  {R.drawable.silver_bee,R.drawable.bronze_bee,R.drawable.silver_bee
+                                    ,R.drawable.silver_bee
+                        ,R.drawable.silver_bee
+                        ,R.drawable.bronze_bee
+                        ,R.drawable.silver_bee
+                        ,R.drawable.gold_bee};
+                        ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(getApplicationContext(),R.layout.item_dialog_my_jewel_list,R.id.iv_jewel,intArray);
+
+                        sp_jewel.setAdapter(adapter);*/
+
+
 
                         builder.setView(v);
                         builder.show();
