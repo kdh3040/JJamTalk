d67b72fdad992a39fdd74a483d6ecd4727402844
MJK-PC\mjk <hngpic@gmail.com>
2018-02-22 00:31:36 +0900

    [호근]디자인 수정중

--- app/src/main/java/com/hodo/talkking/UserPageActivity.java
+++ app/src/main/java/com/hodo/talkking/UserPageActivity.java
@@ -227,7 +227,7 @@ if(mMyData.itemList.get(i) != 0)
             }
  */
         btnRegister = findViewById(R.id.UserPage_btnRegister);
-        btnRegister.setImageResource(mMyData.IsCardList(stTargetData.Idx) ? R.drawable.bee : R.drawable.favorite);
+        btnRegister.setImageResource(mMyData.IsCardList(stTargetData.Idx) ? R.drawable.favor_back : R.drawable.favorite);
         btnRegister.setVisibility(stTargetData.Idx.equals(mMyData.getUserIdx()) ? View.GONE : View.VISIBLE);
         btnGiftHoney =  findViewById(R.id.UserPage_btnGiftHoney);
         btnGiftHoney.setVisibility(stTargetData.Idx.equals(mMyData.getUserIdx()) ? View.GONE : View.VISIBLE);
@@ -303,7 +303,7 @@ if(mMyData.itemList.get(i) != 0)
                                 }
 
 
-                                btnRegister.setImageResource(mMyData.IsCardList(stTargetData.Idx) ? R.drawable.bee : R.drawable.favorite);
+                                btnRegister.setImageResource(mMyData.IsCardList(stTargetData.Idx) ? R.drawable.favor_back : R.drawable.favorite);
                             }
                         };
 
