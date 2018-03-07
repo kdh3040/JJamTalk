7860c6e2fe87e16ed041c8ba3fb4b5d9bba1f74b
MJK-PC\mjk <hngpic@gmail.com>
2018-03-08 02:08:04 +0900

    [호근]디자인변경

--- app/src/main/java/com/hodo/talkking/SettingActivity.java
+++ app/src/main/java/com/hodo/talkking/SettingActivity.java
@@ -2,30 +2,25 @@ package com.hodo.talkking;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.hodo.talkking.Data.MyData;
 import com.hodo.talkking.Data.SettingData;
 import com.hodo.talkking.Firebase.FirebaseData;
@@ -42,16 +37,21 @@ public class SettingActivity extends AppCompatActivity {
     private FirebaseData mFireBaseData = FirebaseData.getInstance();
 
 
-    private TextView  btn_Help;
-    private TextView  btn_LogOut;
-    private TextView  btn_Delete;
-
-    private TextView  btn_Share;
+    private Button  btn_Help;
+    private Button  btn_LogOut;
 
 
-    private LinearLayout lo_recvMsg;
-    private LinearLayout lo_buyGold ;
-    private LinearLayout lo_alarm;
+
+
+
+
+
+    private Button alarm;
+    private Button recvMsg;
+    private Button blockList;
+    private Button share;
+    private Button buyGold;
+    private Button delete;
 
 
     private RadioButton btn_ViewMode_2;
@@ -65,7 +65,7 @@ public class SettingActivity extends AppCompatActivity {
     private Switch sw_AlarmFollow;
     private Switch sw_AlarmLike;
 
-    private TextView tv_blocklist;
+
 
     private CheckBox cbRecvMsg;
 
@@ -109,8 +109,8 @@ public class SettingActivity extends AppCompatActivity {
 
         //imageview 및 textview 초기화
 
-        lo_alarm = findViewById(R.id.ll_alarm);
-        lo_alarm.setOnClickListener(new View.OnClickListener() {
+        alarm = findViewById(R.id.btn_alarm);
+        alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -134,8 +134,8 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
-        lo_recvMsg = findViewById(R.id.lo_recvMsg);
-        lo_recvMsg.setOnClickListener(new View.OnClickListener() {
+        recvMsg = findViewById(R.id.recvMessage);
+        recvMsg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -157,16 +157,16 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
-        tv_blocklist = (TextView)findViewById(R.id.tv_blocklist);
-        tv_blocklist.setOnClickListener(new View.OnClickListener() {
+        blockList = (Button) findViewById(R.id.blocklist);
+        blockList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),BlockListActivity.class));
             }
         });
 
-        btn_Share = (TextView)findViewById(R.id.btn_share);
-        btn_Share.setOnClickListener(new View.OnClickListener() {
+        share = (Button) findViewById(R.id.btn_share);
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = "회원님을 위한 특별한 어플을 발견했습니다.\n톡킹에 로그인해보세요 \n" + mMyData.strDownUri;
@@ -196,8 +196,8 @@ public class SettingActivity extends AppCompatActivity {
                  }
         }) ;*/
 
-        lo_buyGold = findViewById(R.id.lo_buyGold);
-        lo_buyGold.setOnClickListener(new View.OnClickListener() {
+        buyGold = findViewById(R.id.buyCoin);
+        buyGold.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),BuyGoldActivity.class));
@@ -207,8 +207,8 @@ public class SettingActivity extends AppCompatActivity {
         });
 
 
-        btn_Help = (TextView )findViewById(R.id.Setting_btnHelp);
-        btn_LogOut = (TextView )findViewById(R.id.Setting_logout);
+        btn_Help = (Button) findViewById(R.id.btn_help);
+        /*btn_LogOut = (TextView )findViewById(R.id.Setting_logout);
         btn_LogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -223,9 +223,9 @@ public class SettingActivity extends AppCompatActivity {
                 CommonFunc.getInstance().ShowDefaultPopup(SettingActivity.this, listener, "로그아웃", "로그아웃을 하시겠습니까?", "네", "아니요");
             }
         });
-
-        btn_Delete = (TextView )findViewById(R.id.Setting_btnDel);
-        btn_Delete.setOnClickListener(new View.OnClickListener() {
+*/
+        delete = (Button) findViewById(R.id.btn_del);
+        delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
