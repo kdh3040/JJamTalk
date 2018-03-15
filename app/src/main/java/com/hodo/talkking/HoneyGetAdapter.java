9b16a64f592f107aef73c7152dc1e350d97fee17
mangomilk114 <mangomilk114@gmail.com>
2018-03-15 23:22:29 +0900

    [배환웅] 우체통 시간 표기 수정

--- app/src/main/java/com/hodo/talkking/HoneyGetAdapter.java
+++ app/src/main/java/com/hodo/talkking/HoneyGetAdapter.java
@@ -10,6 +10,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hodo.talkking.Data.CoomonValueData;
 import com.hodo.talkking.Data.MyData;
+import com.hodo.talkking.Util.CommonFunc;
 import com.hodo.talkking.ViewHolder.HoneyGetViewHolder;
 
 import java.text.SimpleDateFormat;
@@ -44,9 +45,19 @@ public class HoneyGetAdapter extends RecyclerView.Adapter<HoneyGetViewHolder>{
 
         holder.tv_honeycount.setText(Integer.toString(mMyData.arrRecvHoneyDataList.get(position).nSendHoney));
         holder.tv_nickname.setText(mMyData.arrRecvHoneyDataList.get(position).strTargetNick);
+
+
         Date sendDate = new Date(mMyData.arrRecvHoneyDataList.get(position).strSendDate);
-        SimpleDateFormat ctime = new SimpleDateFormat(CoomonValueData.MAIL_DATE_FORMAT);
-        holder.tv_date.setText(ctime.format(sendDate));
+        if(CommonFunc.getInstance().IsTodayDate(new Date(CommonFunc.getInstance().GetCurrentTime()), sendDate))
+        {
+            SimpleDateFormat ctime = new SimpleDateFormat(CoomonValueData.BOARD_TODAY_DATE_FORMAT);
+            holder.tv_date.setText(ctime.format(sendDate));
+        }
+        else
+        {
+            SimpleDateFormat ctime = new SimpleDateFormat(CoomonValueData.BOARD_DATE_FORMAT);
+            holder.tv_date.setText(ctime.format(sendDate));
+        }
 
         Glide.with(mContext)
                 .load(mMyData.arrRecvHoneyDataList.get(position).strTargetImg)
