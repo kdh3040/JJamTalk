ca76e39d7abf2b2e02e71dd37fc8e4a8aa28e475
kdh3040 <kdh30400@naver.com>
2018-05-31 16:52:38 +0900

    [김도형] 출첵보상 로직 오류 수정

--- app/src/main/java/com/dohosoft/talkking/Firebase/FirebaseData.java
+++ app/src/main/java/com/dohosoft/talkking/Firebase/FirebaseData.java
@@ -537,8 +537,8 @@ public class FirebaseData {
         updateMap.put("NickChangeCnt", mMyData.NickChangeCnt);
         //user.child("NickChangeCnt").setValue(mMyData.NickChangeCnt);
 
-        long time = CommonFunc.getInstance().GetCurrentTime();
-        user.child("ConnectDate").setValue(-1 * time);
+//        long time = CommonFunc.getInstance().GetCurrentTime();
+  //      user.child("ConnectDate").setValue(-1 * time);
 
         user.updateChildren(updateMap);
 
